acd6f75ca695e9d87364db84538cb130fe9c2649
kevin-salamon <kevinwebdevs@gmail.com>
2020-02-11 20:38:48 -0500

    completed basic setup for user prompt in readme generation

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 let inquirer = require('inquirer');
 let fs = require('fs');
+let axios = require('axios');
+
+inquirer
+  .prompt([
+    {
+      type: "input",
+      message: "What is your user name?",
+      name: "username"
+    }
+  ])
+  .then(function(response) {
+
+    fs.writeFile("log.txt", JSON.stringify(response), function(err) {
+        var username = response.username;
+
+        let queryURL = "https://api.github.com/search/users?q=" + username;
+        console.log(queryURL);
+
+        // axios.get(queryURL)
+        // .then(response => {
+        //     console.log(response);
+        // })
+        // .catch(error => {
+        //     console.log(error);
+        // });
+    });
+
+  });
+
+
+
